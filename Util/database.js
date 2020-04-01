c167a7f9751b7e2264db49a684c9b22bb3ffebb9
Athena Capsis <acapsis@AccademiaDegliIncogniti.net>
2020-04-01 14:56:02 -0700

    remove tutorial files

--- Util/database.js
+++ Util/database.js
@@ -4,11 +4,20 @@ const MongoClient = mongodb.MongoClient;
 
 let _db;
 
+// this db connection is for mongo hosted by mongo atlas
+//'mongodb+srv://Anemone:traveloggia_256@cluster0-9kdvr.mongodb.net/traveloggia?retryWrites=true' , { useNewUrlParser: true })
+// reseller of aws or azure or google hosting services
+// but it costs money - try for free of course
+// so instead we are going to host mongo along with the node api
+// in digital ocean which we are paying for...
+// when we scale to empire, we can pay for db hosting service for now we will host our own 
+
+
 const mongoConnect = (callback) => {
     // db name is in the connection string - creates it if doesnt exist yet
-    MongoClient.connect('mongodb+srv://Anemone:traveloggia_256@cluster0-9kdvr.mongodb.net/traveloggia?retryWrites=true' , { useNewUrlParser: true })
+    MongoClient.connect('mongodb://localhost:27017/traveloggiaDB')
     .then( client => {
-        console.log('connected')
+        console.log('mongo traveloggiaDB connected')
         // you can pass in the name of the db you want to use
 
         _db = client.db()
