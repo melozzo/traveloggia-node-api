551a7ebd53cea4f1dfce807457430bd7ac676897
Athena Capsis <acapsis@accademiaDegliIncogniti.net>
2020-04-16 20:54:36 -0700

    huh?

--- Models/photo.js
+++ Models/photo.js
@@ -12,7 +12,7 @@ const photoSchema = new Schema({
         Caption:{type:String},
         IsDeleted:{type:Boolean, default:false}
 
-}, {collection:"Photos", timestamps:true});
+}, {collection:"Photos", timestamps:true})
 
 
 module.exports = mongoose.model('Photo', photoSchema)
