a0474866104fb26f2cec39b8e1e0949330aa52b6
Athena Capsis <acapsis@accademiaDegliIncogniti.net>
2021-04-09 19:57:06 -0700

    put site

--- Routes/siteRoutes.js
+++ Routes/siteRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/list/:mapId', siteController.getList);
 
 router.get('/:siteId', siteController.getSite);
-router.post('/update', siteController.updateSite);
+router.put('/update', siteController.updateSite);
 router.post('/create', siteController.createSite);
 
 
