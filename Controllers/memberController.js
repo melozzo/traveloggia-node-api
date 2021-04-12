1a8fc1f521050d0a904c19ff4580372e0399a323
Athena Capsis <acapsis@accademiaDegliIncogniti.net>
2021-04-11 20:04:59 -0700

    prevent accidental empty login

--- Controllers/memberController.js
+++ Controllers/memberController.js
@@ -2,18 +2,22 @@ const Member = require('../Models/member');
 const moment = require('moment');
 
 exports.login= ( req, res, next)=>{      
-        let email = req.body.Email;
-        let pwd = req.body.Password;
 
-        Member.findOne({"Email":email,"Password":pwd})
-        .then( member =>{
+      let email = req.body.Email;
+      let pwd = req.body.Password;
+      console.log('api method login entered', email, pwd)
+      if( ! email || ! pwd)
+            throw new Error("email and or password undefined")
+      Member.findOne({"Email":email,"Password":pwd})
+      .then( member =>{
             if(! member)
                   throw new Error("member not found");
+                  console.log(member)
             res.status(200).json(member)
-        })
-        .catch(error=>{
+            })
+      .catch(error=>{
             res.status(500).json(JSON.stringify(error))
-        })
+      })
 }
 
 exports.create = ( req, res, next)=>{
